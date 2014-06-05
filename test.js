dcc8de7d78d9b3ce13860adfdd5269f5631e0ccd
Ragesh Krishna <ragesh@rageshkrishna.com>
2014-06-05 17:36:30 +0530

    Fixed unit tests

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 var superagent = require("superagent"),
     chai = require("chai"),
-    expect = chai.expect;
+    expect = chai.expect,
+    should = require("should");
 
 describe("Index", function () {
   it("renders HTML", function () {
     superagent.get("http://localhost:3000/")
       .end(function (e, res) {
-        e.should.equal(null);
-        var response = res.body.length > 0;
-        response.should.equal(true);
+        (e === null).should.equal(true);
+        res.text.should.equal("Hey buddy!");
       });
   });
 });
@@ -17,7 +17,7 @@ describe("Persistence", function () {
   it("should create a thing", function () {
     superagent.get("http://localhost:3000/doobie")
       .end(function (e, res) {
-        e.should.equal(null);
+        (e === null).should.equal(true);
         var response = (res.body.indexOf("new") !== -1);
         expect(response).to.equal(false);
       });
@@ -25,7 +25,7 @@ describe("Persistence", function () {
   it("should retrieve a thing", function () {
     superagent.get("http://localhost:3000/doobie")
       .end(function (e, res) {
-        e.should.equal(null);
+        (e === null).should.equal(true);
         var response = JSON.parse(res.body);
         response = response[0];
         response.should.have.property("name", "doobie");
