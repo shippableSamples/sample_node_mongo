7671e9645af1b19df350e92282e877ccb18a109c
Ragesh Krishna <ragesh@rageshkrishna.com>
2014-06-05 18:06:02 +0530

    Made index test also async

--- test.js
+++ test.js
@@ -4,11 +4,12 @@ var superagent = require("superagent"),
     should = require("should");
 
 describe("Index", function () {
-  it("renders HTML", function () {
+  it("renders HTML", function (done) {
     superagent.get("http://localhost:3000/")
       .end(function (e, res) {
         (e === null).should.equal(true);
         res.text.should.equal("Hey buddy!");
+        done();
       });
   });
 });
